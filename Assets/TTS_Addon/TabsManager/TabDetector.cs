0936b8c26433a717538cfae39e48c0c31607af71
Salvi <salvio.calvillo@enti.cat>
2022-03-03 13:55:09 +0100

    No puedo abrir ventanas internas comodamente
    Inicio del keyhandler

--- Assets/TTS_Addon/TabsManager/TabDetector.cs
+++ Assets/TTS_Addon/TabsManager/TabDetector.cs
@@ -11,43 +11,104 @@ static class SceneDetector
     static EditorWindow previousWindow, currentWindow;
     static string nameOfCurrentWindow = "";
     static bool started = false;
+
+    static bool pressedControl = false;
+
     static SceneDetector()
     {
         EditorApplication.update += Update;
+
+
+        System.Reflection.FieldInfo info = typeof(EditorApplication)
+            .GetField("globalEventHandler",
+            System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+        EditorApplication.CallbackFunction value = (EditorApplication.CallbackFunction)info.GetValue(null);
+
+        value += EditorGlobalKeyPress;
+
+        info.SetValue(null, value);
+
         // Aquí los mencionará todos,
         // pero tengo que mirar que pasa
         // si le van a dar al play cada
         // 1000 veces, o si se recompilarán los scripts
-        
-    }
 
+
+    }
+    static void EditorGlobalKeyPress()
+    {
+        Event current = Event.current;
+
+        if (Event.current.keyCode == KeyCode.LeftControl) // TODO cambiar esto para que sea 1 click
+        {
+            pressedControl = true;
+        }
+        
+        if (!pressedControl) return;
+        switch (Event.current.keyCode)
+        {
+            case KeyCode.Alpha1:
+                Debug.Log("Scene");
+                break;
+            case KeyCode.Alpha2:
+                Debug.Log("Game");
+                break;
+            case KeyCode.Alpha3:
+                Debug.Log("Inspector");
+                break;
+            case KeyCode.Alpha4:
+                Debug.Log("Hierarchy");
+                break;
+            case KeyCode.Alpha5:
+                Debug.Log("Project");
+                break;
+            case KeyCode.Escape:
+                Debug.Log("Console");
+                break;
+            default:
+                break;
+        }
+    }
     static void Update()
     {
-
+       
         if (currentWindow == null && !started)
         {
             started = true;
             EditorWindow[] allWindows = Resources.FindObjectsOfTypeAll<EditorWindow>();
-            Debug.Log("Windows Size: " + allWindows.Length);
-       
-            ShowEditorWindowWithTypeName(Windows.SceneView);
+            ShowSceneEditorWindow();
             currentWindow = EditorWindow.focusedWindow;
-            Debug.Log(currentWindow.GetType().Name); 
         }else if(currentWindow == null && started)
         {
-            Debug.Log("a");
+            //Debug.Log("a"); 
         }
-        WindowsChecker(); 
+        currentWindow = EditorWindow.focusedWindow;
+        if (currentWindow != null)
+        {
+            WindowsChecker();
+        }
+        else
+        {
+            if(previousWindow != null)
+                Debug.Log("Nothing Selected");
+            previousWindow = null;
+        }
+
+      
+
+
     }
-    
+
+
+
+
     static void WindowsChecker()
     {
-        currentWindow = EditorWindow.focusedWindow;
         if (currentWindow != previousWindow)
         {
-            Debug.Log(currentWindow);
             nameOfCurrentWindow = currentWindow.GetType().Name;
             Debug.Log(nameOfCurrentWindow);
+
             switch (nameOfCurrentWindow)
             {
                case Windows.SceneHierarchyWindow:  
@@ -63,7 +124,8 @@ static class SceneDetector
                 case Windows.ProjectBrowser:
                     break;
                 default:
-                    Debug.Log(nameOfCurrentWindow);
+                    Debug.Log("Unsuported by voiceOver: " + nameOfCurrentWindow);
+
                     break;
             }
         }
@@ -86,14 +148,12 @@ static class SceneDetector
  
     static void ShowInspectorEditorWindow()
     {
-        string inspectorWindowTypeName = "UnityEditor.InspectorWindow";
-        ShowEditorWindowWithTypeName(inspectorWindowTypeName);
+        ShowEditorWindowWithTypeName(Windows.InspectorWindow);
     }
 
     static void ShowSceneEditorWindow()
     {
-        string sceneWindowTypeName = "UnityEditor.SceneView";
-        ShowEditorWindowWithTypeName(sceneWindowTypeName);
+        ShowEditorWindowWithTypeName(Windows.SceneView);
     }
 
     static void ShowEditorWindowWithTypeName(string windowTypeName)
@@ -106,42 +166,59 @@ static class SceneDetector
             type.IsSubclassOf(typeof(EditorWindow))).ToArray();
 
         var window = EditorWindow.GetWindow<UnityEditor.SceneView>(types);
-        */
-
         
+        Debug.Log(string.Join("\n",
+            AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => type.IsClass && !type.IsAbstract 
+            && type.IsSubclassOf(typeof(EditorWindow))).Select(t => t.FullName)));
+        */
         var types = new List<Type>()
         { 
             // first add your preferences
             typeof(SceneView),
-            typeof(Editor).Assembly.GetType("UnityEditor." + windowTypeName)
+            typeof(Editor).Assembly.GetType("UnityEditor.GameView"),
+            typeof(Editor).Assembly.GetType("UnityEditor.SceneHierarchyWindow"),
+            typeof(Editor).Assembly.GetType("UnityEditor.ConsoleWindow"),
+            typeof(Editor).Assembly.GetType("UnityEditor.ProjectBrowser"),
+            typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow")
         };
-
+        /*
         // and then add all others as fallback (who cares about duplicates at this point ? ;) )
         types.AddRange(AppDomain.CurrentDomain.GetAssemblies().
-            SelectMany(assembly => assembly.GetTypes())
-            .Where(type => type.IsClass && !type.IsAbstract 
-            && type.IsSubclassOf(typeof(EditorWindow))));
-
+        SelectMany(assembly => assembly.GetTypes())
+        .Where(type => type.IsClass && !type.IsAbstract 
+        && type.IsSubclassOf(typeof(EditorWindow))));
+        */
+   
         switch (windowTypeName)
         {
             case Windows.SceneHierarchyWindow:
+                var hWindow = EditorWindow.GetWindow(typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow"));
+                hWindow.Focus();
                 break;
             case Windows.SceneView:
-                var window = EditorWindow.GetWindow<UnityEditor.SceneView>(types.ToArray());
-                window.Focus();
+                var sWindow = EditorWindow.GetWindow(typeof(SceneView));
+                sWindow.Focus();
                 break;
             case Windows.GameView:
-                window = EditorWindow.GetWindow<UnityEditor.SceneView>(types.ToArray());
-                window.Focus();
+                var gWindow = EditorWindow.GetWindow(typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow"));
+                gWindow.Focus();
                 break;
             case Windows.ConsoleWindow:
+                var cWindow = EditorWindow.GetWindow(typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow"));
+                cWindow.Focus();
                 break;
             case Windows.InspectorWindow:
+                var iWindow = EditorWindow.GetWindow(typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow"));
+                iWindow.Focus();
                 break;
             case Windows.ProjectBrowser:
+                var pWindow = EditorWindow.GetWindow(typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow"));
+                pWindow.Focus();
                 break;
             default:
-                Debug.Log(nameOfCurrentWindow);
+                Debug.Log("You're trying to open a non-supported window");
                 break;
 
         }
