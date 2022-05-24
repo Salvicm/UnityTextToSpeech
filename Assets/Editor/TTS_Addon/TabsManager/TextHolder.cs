fcfe48ee045b1d70f4932afb1e21d9c1e97e6fc0
Salvi <salvio.calvillo@enti.cat>
2022-05-24 18:33:32 +0200

    Traducido al inglés

--- Assets/Editor/TTS_Addon/TabsManager/TextHolder.cs
+++ Assets/Editor/TTS_Addon/TabsManager/TextHolder.cs
@@ -4,95 +4,80 @@ using UnityEditor;
 
 static class TextHolder
 {
+
     // Base
-    public const string InitializingTTS = "Inicializando TTS";
-    public const string voidText = "";
-    
+    public static string InitializingTTS = "Initializing TTS";
+    public static string voidText = "";
+
     // OpenWindows
-    public const string OpenHierarchy = "Abriendo Jerarquía";
-    public const string OpenScene = "Abriendo escena";
-    public const string OpenGame = "Abriendo juego";
-    public const string OpenConsole = "Abriendo consola";
-    public const string OpenInspector = "Abriendo inspector";
-    public const string OpenExplorer = "Abriendo explorador de archivos";
-    public const string OpeningUnsuportedScene = "Abriendo ventana no soportada por el TTS: ";
+    public static string OpenHierarchy = "Opening hierarchy";
+    public static string OpenScene = "Opening scene";
+    public static string OpenGame = "Opening game";
+    public static string OpenConsole = "Opening console";
+    public static string OpenInspector = "Opening inspector";
+    public static string OpenExplorer = "Opening file explorer";
+    public static string OpeningUnsuportedScene = "Opening unsuported window: ";
 
 
     //Hierarchy controller
-    public const string ChildrenList = "tiene un total de ";
-    public const string Children = "hijos";
-    public const string HasParent = "Tiene padre";
-    public const string IsAPrefab = "Es un prefab";
-    public const string NotAPrefab = "No es un prefab";
-    public const string CurrentlyEditingPrefab = "Editando el prefab: ";
+    public static string ChildrenList = "has a total of ";
+    public static string Children = "children";
+    public static string HasParent = "Has a parent";
+    public static string IsAPrefab = "Is a prefab";
+    public static string NotAPrefab = "Is not a prefab";
+    public static string CurrentlyEditingPrefab = "Editing prefab: ";
 
     // project controller
-    public const string LocationIs = "La direccion completa es: ";
-    public const string ActualFolder = "La carpeta actual es: ";
-    public const string ThisItemIs = " es del tipo ";
-    public const string IsAScript = "Script";
-    public const string IsAFolder = "Carpeta";
-    public const string IsAScene = "Escena";
-    public const string IsAnAudioMixer = "Controlador de mezclador de audio";
-    public const string IsAShader = "Shader";
-    public const string IsAGameObject = "Game Object";
-    public const string IsADLL = "DLL";
-    public const string UnknownItem = "Desconocido";
+    public static string LocationIs = "Complete path is: ";
+    public static string ActualFolder = "Actual folder is: ";
+    public static string ThisItemIs = " is type ";
+    public static string IsAScript = "Script";
+    public static string IsAFolder = "folder";
+    public static string IsAScene = "scene";
+    public static string IsAnAudioMixer = "Audio Mixer controller";
+    public static string IsAShader = "Shader";
+    public static string IsAGameObject = "Game Object";
+    public static string IsADLL = "DLL";
+    public static string UnknownItem = "Unknown";
 
     // Console 
-    public const string OnlyErrors = "Solo errores";
-    public const string OnlyWarnings = "Solo advertencias";
-    public const string OnlyLogs = "Solo logs";
-    public const string OnlyExceptions = "Solo excepciones";
-    public const string OnlyAsserts = "Solo asserts";
-    public const string All = "Todos";
+    public static string OnlyErrors = "Only errors";
+    public static string OnlyWarnings = "Only warnings";
+    public static string OnlyLogs = "Only logs";
+    public static string OnlyExceptions = "Only exceptions";
+    public static string OnlyAsserts = "Only asserts";
+    public static string All = "All";
+       
 
-    public const string readingAll = "Leyendo el stacktrace completo: ";
-    public const string ErrorNumber = "Error número ";
-    public const string Of = " de ";
+    public static string readingAll = "Reading full stacktrace: ";
+    public static string ErrorNumber = "Error number ";
+    public static string Of = " of ";
 
-    [MenuItem("TTS/test %&J")]
-    static void LoadTTS()
-    {
-        char t = "a"[2];
-    }
+
     //Info
-    public const string HierarchyInfo = "Con el botón general puedes saber que objeto está seleccionado, si tiene padre y cuantos hijos. " +
-        "Usa el botón de avance para oir el número de hijos que tiene y enumerarlos" +
-        "Usa el botón de retroceso para oir el nombre del objeto padre en caso de tenerlo" +
-        "Usa el botón Auxiliar 1 para saber que hijos tiene ese objeto. " +
-        "Usa el botón auxiliar 2 para <no implementado>. " +
-        "Usa el botón auxiliar 3 para saber si el objeto pertenece a un prefab y, si estás editando uno, cual es.";
-    public const string ProjectFolderInfo = "Con el botón general puedes saber que objeto está seleccionado y su extensión." +
-        "Usa el botón de avance para <no implementado>" +
-        "Usa el botón de retroceso para <no implementado>" +
-        "Usa el botón Auxiliar 1 para saber la dirección completa del objeto seleccionado." +
-        "Usa el botón Auxiliar 2 para saber la carpeta actual del objeto seleccionado." +
-        "Usa el botón auxiliar 3 para saber si el objeto es un prefab.";
-    public const string InspectorInfo = "Con el botón general puedes <no implementado>." +
-        "Usa el botón de avance para <no implementado>" +
-        "Usa el botón de retroceso para <no implementado>" +
-        "Usa el botón Auxiliar 1 para <no implementado>." +
-        "Usa el botón Auxiliar 2 para <no implementado>." +
-        "Usa el botón auxiliar 3 para <no implementado>.";
-    public const string GameInfo = "Con el botón general puedes <no implementado>." +
-        "Usa el botón de avance para <no implementado>" +
-        "Usa el botón de retroceso para <no implementado>" +
-        "Usa el botón Auxiliar 1 para <no implementado>." +
-        "Usa el botón Auxiliar 2 para <no implementado>." +
-        "Usa el botón auxiliar 3 para <no implementado>.";
-    public const string ConsoleInfo = "Con el botón general puedes saber el log actual, lo que dice y el tipo." +
-        "Usa el botón de avance para avanzar al siguiente Log" +
-        "Usa el botón de retroceso para retroceder al log anterior" +
-        "Usa el botón Auxiliar 1 para escuchar el path del log actual." +
-        "Usa el botón Auxiliar 2 para escuchar el stacktrace completo." +
-        "Usa el botón auxiliar 3 para modificar el tipo de logs que quieres revisar.";
-    public const string SceneInfo = "Con el botón general puedes <no implementado>." +
-        "Usa el botón de avance para <no implementado>" +
-        "Usa el botón de retroceso para <no implementado>" +
-        "Usa el botón Auxiliar 1 para <no implementado>." +
-        "Usa el botón Auxiliar 2 para <no implementado>." +
-        "Usa el botón auxiliar 3 para <no implementado>.";
+    public static string HierarchyInfo = "With the general button you can know which object is selected, if it has a parent and how many children does it have. " +
+        "Use advance button to hear the number of childs it has and enumerate them." +
+        "Use regress button to hear the name of the parent object in case it exists." +
+        "Use auxiliar button 1 to saber que hijos tiene ese objeto. " +
+        "Use auxiliar button 2 to . " +
+        "Use auxiliar button 3 to know if the object belongs to a prefab and, in case you're in edit mode, which one.";
+  
+    public static string ProjectFolderInfo = "With the general button you know what object is selected and its extension." +
+        "Use advance button to " +
+        "Use regress button to " +
+        "Use auxiliar button 1 to know the selected object's full path." +
+        "Use auxiliar button 2 to know the folder of the current selected object." +
+        "Use auxiliar button 3 to know if the object is a prefab.";
+
+    public static string InspectorInfo = "Unimplemented window";
+    public static string GameInfo = "Unimplemented window";
+    public static string ConsoleInfo = "With the general button you can know the actual log, what it says and the type." +
+        "Use advance button to advance to the next log." +
+        "Use regress button to return to the previous log." +
+        "Use auxiliar button 1 to hear actual log path." +
+        "Use auxiliar button 2 to hear the full stacktrace." +
+        "Use auxiliar button 3 to modify the type of logs to check'.";
+    public static string SceneInfo = "Unimplemented window";
 
 
 
